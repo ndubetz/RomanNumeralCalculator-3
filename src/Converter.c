c70f3e3edf0dd246ecd5962610be0b10f1887c09
NDUB <ndubetz@pillartechnology.com>
2016-08-10 20:43:16 -0400

    convert single digits to numerals

--- src/Converter.c
+++ src/Converter.c
@@ -3,7 +3,31 @@
 
 #include "Converter.h"
 
+struct arabic_to_roman 
+{
+	const int arabic;
+	const char * roman;
+};
+
+struct arabic_to_roman ARABIC_TO_ROMAN[] =
+{
+    {1000,  "M"},
+    { 500,  "D"},
+    { 100,  "C"},
+    {  50,  "L"},
+    {  10,  "X"},
+    {   5,  "V"},
+    {   1,  "I"}
+};
+
 void Converter_arabic_to_roman(char * numeral, int number)
 {
-	strcat(numeral, "I");
+	int i;
+	for(i = 0; i < 7; i++)
+	{
+		if (number == ARABIC_TO_ROMAN[i].arabic) 
+		{
+			strcat(numeral, ARABIC_TO_ROMAN[i].roman);			
+		}
+	}
 }
--- test/check_Converter.c
+++ test/check_Converter.c
@@ -5,13 +5,23 @@
 #include "check_Converter.h"
 #include "../src/Converter.h"
 
-
-START_TEST (convert_to_single_roman_digits)
+void test_arabic_to_roman(int number, char * expectedResult)
 {
 	char buffer[30];
 	memset(buffer, 0, 30);
-	Converter_arabic_to_roman(buffer, 1);
-	ck_assert_str_eq(buffer, "I");
+	Converter_arabic_to_roman(buffer, number);
+	ck_assert_str_eq(buffer, expectedResult);
+}
+
+START_TEST (convert_to_single_roman_digits)
+{
+	test_arabic_to_roman(1,    "I");
+	test_arabic_to_roman(5,    "V");
+	test_arabic_to_roman(10,   "X");
+	test_arabic_to_roman(50,   "L");
+	test_arabic_to_roman(100,  "C");
+	test_arabic_to_roman(500,  "D");
+	test_arabic_to_roman(1000, "M");
 }
 END_TEST
 
