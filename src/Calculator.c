b9d9abc6e469ebd683f703e2f19bfc4e1c5d003f
NDUB <ndubetz@pillartechnology.com>
2016-08-17 00:20:23 -0400

    added more addition test cases

--- src/Calculator.c
+++ src/Calculator.c
@@ -6,6 +6,7 @@
 
 void Calculator_add(char * buffer, char * numeral1, char * numeral2)
 {
-	Converter_arabic_to_roman(buffer, (Converter_roman_to_arabic(numeral1) + Converter_roman_to_arabic(numeral2)));
+	int sum = Converter_roman_to_arabic(numeral1) + Converter_roman_to_arabic(numeral2);
+	Converter_arabic_to_roman(buffer, sum);
 }
 
--- test/check_Calculator.c
+++ test/check_Calculator.c
@@ -5,13 +5,23 @@
 #include "check_Calculator.h"
 #include "../src/Calculator.h"
 
-
-START_TEST (Calculator_add_basic_cases)
+void test_addition(char * numeral1, char * numeral2, char * result)
 {
 	char buffer[50];
 	memset(buffer, 0, 50);
-	Calculator_add(buffer, "I", "I");
-	ck_assert_str_eq(buffer, "II");
+	Calculator_add(buffer, numeral1, numeral2);
+	ck_assert_str_eq(buffer, result);
+}
+
+START_TEST (Calculator_adds_two_numerals)
+{
+	test_addition("MCXI", "MMXXCCII", "MMMCCCXXXIII");
+	test_addition("CCXXII", "CCXXII", "CDXLIV");
+	test_addition("CCCXXXIII", "CCCXXXIII", "DCLXVI");
+	test_addition("CDXLIV", "CCXXII", "DCLXVI");
+	test_addition("DCLXVI", "DCLXVI", "MCCXXII");
+	test_addition("CMCXIX", "DLV", "MCDXLIV");
+	test_addition("MDCLXVI", "CCCXXXIII", "MCMXCIX");
 }
 END_TEST
 
@@ -19,7 +29,7 @@ TCase * CalculatorTests()
 {
 	TCase * tc;
 	tc = tcase_create("CalculatorTests");
-	tcase_add_test(tc, Calculator_add_basic_cases);
+	tcase_add_test(tc, Calculator_adds_two_numerals);
 	return tc;
 }
 
