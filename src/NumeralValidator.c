a7a9d0e46781dbe5504317a647e0855a6193bbd8
NDUB <ndubetz@pillartechnology.com>
2016-09-01 22:36:52 -0400

    refactor Validator

--- src/NumeralValidator.c
+++ src/NumeralValidator.c
@@ -4,14 +4,14 @@
 
 #include "NumeralValidator.h"
 
-const char * VALID_ROMAN_NUMERALS = "IVXLCDM";
+const char * VALID_ROMAN_NUMERAL_DIGITS = "IVXLCDM";
 
-static bool is_valid_numeral(char numeral)
+static bool is_valid_numeral_digit(char digit)
 {
 	int i;
-	for(i = 0; i < strlen(VALID_ROMAN_NUMERALS); i++)
+	for(i = 0; i < strlen(VALID_ROMAN_NUMERAL_DIGITS); i++)
 	{
-		if(numeral == VALID_ROMAN_NUMERALS[i])
+		if(digit == VALID_ROMAN_NUMERAL_DIGITS[i])
 		{
 			return true;
 		}
@@ -19,16 +19,24 @@ static bool is_valid_numeral(char numeral)
 	return false;
 }
 
-bool NumeralValidator_isValid(const char * numeral)
+static bool are_all_valid_numerals(const char * numeral)
 {
-
 	int i;
 	for(i = 0; i < strlen(numeral); i++)
 	{
-		if(!is_valid_numeral(numeral[i]))
+		if(!is_valid_numeral_digit(numeral[i]))
 		{
 			return false;
 		}
+	}
+	return true;
+} 
+
+bool NumeralValidator_isValid(const char * numeral)
+{
+	if(!are_all_valid_numerals(numeral))
+	{
+		return false;
 	}
 	return true;
 }
